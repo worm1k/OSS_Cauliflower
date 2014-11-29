e0a1a198c5bc38c20a19789a79cd386d9dbc3c44
kebal <nirvana.22@mail.ru>
2014-11-29 18:59:16 +0200

    #2

--- webapp/src/com/naukma/cauliflower/controllers/GetTasksController.java
+++ webapp/src/com/naukma/cauliflower/controllers/GetTasksController.java
@@ -1,10 +1,15 @@
 package com.naukma.cauliflower.controllers;
 
+import com.naukma.cauliflower.dao.TaskStatus;
+import com.naukma.cauliflower.entities.Task;
+import com.naukma.cauliflower.entities.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Алексей on 29.11.2014.
@@ -16,7 +21,11 @@ public class GetTasksController {
     SOW.4
     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        User user = (User) request.getSession().getAttribute("user");
+        //get tasks with taskStatusId TaskStatus.FREE and userRole
+        //List<Task> tasks = getTasksByStatusAndRole(TaskStatus.FREE, user.getUserRoleId());
+        //request.setAttribute("tasks", tasks);
+        //request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
