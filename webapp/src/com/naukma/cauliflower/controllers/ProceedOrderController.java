6166f6316dce2806c063956f252309f4fc837ad0
Maxim Hladchuk <maxlimonad@gmail.com>
2014-11-29 12:35:10 +0200

    new files

---
+++ webapp/src/com/naukma/cauliflower/controllers/ProceedOrderController.java
@@ -0,0 +1,26 @@
+package com.naukma.cauliflower.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Max on 29.11.2014.
+ */
+@WebServlet(name = "ProceedOrderController")
+public class ProceedOrderController extends HttpServlet {
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
+    }
+}
