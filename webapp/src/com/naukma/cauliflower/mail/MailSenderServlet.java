3071d79e0b231a5f728f5cf4dc9ab902ec8c5da3
kemiKondratenko <kemi.kondratenko@gmail.com>
2014-12-01 01:23:33 +0200

    commit to pull last version, nothing changed

--- webapp/src/com/naukma/cauliflower/mail/MailSenderServlet.java
+++ webapp/src/com/naukma/cauliflower/mail/MailSenderServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.Writer;
 
 /**
  * Created by Max on 27.11.2014.
@@ -23,7 +22,8 @@ public class MailSenderServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ServletContext context = getServletContext();
         String fullPath = context.getRealPath("/WEB-INF/mail/");
-        EmailSender.sendEmail((User) request.getSession().getAttribute("user"), "gg", "gg", EmailSender.getTemplate("/html-mail-template.ftl", fullPath));
+        EmailSender.sendEmail((User) request.getSession().getAttribute("user"), "gg", "gg",
+                EmailSender.getTemplate("/html-mail-template.ftl", fullPath));
 
 
     }
