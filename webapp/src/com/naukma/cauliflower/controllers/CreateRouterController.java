bdaa19fc8e90474a5bbdddab39642bfdf1350d0f
nikitinalexx <nirvana.22@mail.ru>
2014-12-01 19:03:28 +0200

    Added extends HttpServlet

--- webapp/src/com/naukma/cauliflower/controllers/CreateRouterController.java
+++ webapp/src/com/naukma/cauliflower/controllers/CreateRouterController.java
@@ -7,6 +7,7 @@ import com.naukma.cauliflower.entities.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -16,7 +17,7 @@ import java.io.IOException;
  */
 
 @WebServlet(name = "CreateRouterController")
-public class CreateRouterController {
+public class CreateRouterController extends HttpServlet {
 
     /*
     RI.9
