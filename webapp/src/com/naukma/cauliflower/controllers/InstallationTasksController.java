ad5f47040a28702f4b1fbee176097f595e3ebb57
nikitinalexx <nirvana.22@mail.ru>
2014-12-02 12:45:13 +0200

    #6

--- webapp/src/com/naukma/cauliflower/controllers/InstallationTasksController.java
+++ webapp/src/com/naukma/cauliflower/controllers/InstallationTasksController.java
@@ -30,24 +30,27 @@ public class InstallationTasksController extends HttpServlet {
         Task task = (Task) request.getAttribute("task");
         int taskId = task.getTaskId();
         int serviceOrderId = task.getServiceOrderId();
-
         //RI.9
         //The system should allow creating Devices, Ports and Cables only by Installation Engineer
-        if(user.getUserRoleId() == DAO.INSTANCE.getUserRoleIdFor_InstallationEngineer()) {
-            Scenario scenario = DAO.INSTANCE.getOrderScenario(serviceOrderId);
-            if (scenario == Scenario.NEW) {
-                if (!DAO.INSTANCE.freePortExists())
-                    DAO.INSTANCE.createRouter();
-                DAO.INSTANCE.createPortAndCableAndAssignToServiceInstance(serviceOrderId);
-            } else if (scenario == Scenario.DISCONNECT) {
-                DAO.INSTANCE.removeCableFromServiceInstanceAndFreePort(serviceOrderId);
-            }
-            DAO.INSTANCE.changeTaskStatus(taskId, TaskStatus.COMPLETED);
-            DAO.INSTANCE.createTaskForProvisioning(serviceOrderId);
+        if (DAO.INSTANCE.getTaskStatus(taskId) == TaskStatus.PROCESSING) {
+            if(user.getUserRoleId() == DAO.INSTANCE.getUserRoleIdFor_InstallationEngineer()) {
+                Scenario scenario = DAO.INSTANCE.getOrderScenario(serviceOrderId);
+                if (scenario == Scenario.NEW) {
+                    if (!DAO.INSTANCE.freePortExists())
+                        DAO.INSTANCE.createRouter();
+                    DAO.INSTANCE.createPortAndCableAndAssignToServiceInstance(serviceOrderId);
+                } else if (scenario == Scenario.DISCONNECT) {
+                    DAO.INSTANCE.removeCableFromServiceInstanceAndFreePort(serviceOrderId);
+                }
+                DAO.INSTANCE.changeTaskStatus(taskId, TaskStatus.COMPLETED);
+                DAO.INSTANCE.createTaskForProvisioning(serviceOrderId);
 
-            request.getRequestDispatcher("smthing.jsp?created=true").forward(request, response);
-        }else
-            request.getRequestDispatcher("smthing.jsp?created=you%20have%20no%20rihts%20for%20that").forward(request, response);
+                request.getRequestDispatcher("smthing.jsp?created=true").forward(request, response);
+            }else
+                request.getRequestDispatcher("smthing.jsp?created=you%20have%20no%20rihts%20for%20that").forward(request, response);
+        } else
+            request.getRequestDispatcher("taskalreadycompleted.jsp").forward(request, response);
+
 
     }
 
