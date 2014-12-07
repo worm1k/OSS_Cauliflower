5b76624c25c7326743767b35aea445e4c1399ae9
nikitinalexx <nirvana.22@mail.ru>
2014-12-08 01:08:59 +0200

    Added junit jdbc oracle source lookup

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,15 +1,47 @@
 import com.naukma.cauliflower.dao.DAO;
 import com.naukma.cauliflower.dao.UserRoles;
 import com.naukma.cauliflower.entities.User;
+import oracle.jdbc.pool.OracleConnectionPoolDataSource;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.sql.SQLException;
+
 
 /**
  * Created by Max on 07.12.2014.
  */
 
 public class UserTest {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        try {
+            // Create initial context
+            System.setProperty(Context.INITIAL_CONTEXT_FACTORY,
+                    "org.apache.naming.java.javaURLContextFactory");
+            System.setProperty(Context.URL_PKG_PREFIXES,
+                    "org.apache.naming");
+            InitialContext ic = new InitialContext();
+
+            ic.createSubcontext("jdbc");
+            OracleConnectionPoolDataSource ds = new OracleConnectionPoolDataSource();
+            // Construct DataSource
+
+            ds.setURL("jdbc:oracle:thin:@localhost:1521/xe");
+            ds.setUser("CAULIFLOWER");
+            ds.setPassword("11111");
+
+            ic.bind("jdbc/oraclesource", ds);
+        } catch (NamingException ex) {
+            System.out.println("Failed to start junit testing");
+            ex.printStackTrace();
+        }
+    }
 
     @Test
     public void testUser() throws SQLException {
