bd5a93dba64b46e7a512bbd11acde77262c25456
oliynuk25 <oliynuk25@gmail.com>
2014-12-15 00:32:54 +0200

    CustomerSupportEngineerFilter
    + web xml

---
+++ webapp/src/com/naukma/cauliflower/filters/CustomerSupportEngineerFilter.java
@@ -0,0 +1,40 @@
+package com.naukma.cauliflower.filters;
+
+import com.naukma.cauliflower.dao.UserRole;
+import com.naukma.cauliflower.entities.User;
+import com.naukma.cauliflower.info.CauliflowerInfo;
+import org.apache.log4j.Logger;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by ihor on 12.12.2014.
+ */
+public class CustomerSupportEngineerFilter extends BaseFilter {
+    private static final Logger logger = Logger.getLogger(InstallationEngineerFilter.class);
+    @Override
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) {
+        User user = (User)request.getSession().getAttribute(CauliflowerInfo.USER_ATTRIBUTE);
+        if(user==null || (user!=null && !user.getUserRole().equals(UserRole.CUST_SUP_ENG.toString()))) {
+            try {
+                logger.info("CustomerSupportEngineerFilter ::  user has not permission to see customer support engineer page");
+                response.sendRedirect(CauliflowerInfo.HOME_LINK);
+            } catch (IOException e) {
+                logger.error(e);
+            }
+        }else{
+            try {
+                logger.info("CustomerSupportEngineerFilter ::  user has  permission to see customer support engineer page");
+                chain.doFilter(request,response);
+            } catch (IOException e) {
+                logger.error(e);
+            } catch (ServletException e) {
+                logger.error(e);
+            }
+        }
+    }
+}
