25e30e63997e72b5b38efba8f1d500488d9e94a6
oliynuk25 <oliynuk25@gmail.com>
2014-12-15 00:32:06 +0200

    AdminFilter
    add

---
+++ webapp/src/com/naukma/cauliflower/filters/AdminFilter.java
@@ -0,0 +1,42 @@
+package com.naukma.cauliflower.filters;
+
+import com.naukma.cauliflower.dao.UserRole;
+import com.naukma.cauliflower.entities.User;
+import com.naukma.cauliflower.info.CauliflowerInfo;
+import org.apache.log4j.Logger;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by ihor on 11.12.2014.
+ */
+@WebFilter(filterName = "AdminFilter",urlPatterns = "/admdashboard.jsp")
+public class AdminFilter extends BaseFilter {
+    private static final Logger logger = Logger.getLogger(AdminFilter.class);
+    @Override
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) {
+        User user = (User)request.getSession().getAttribute(CauliflowerInfo.USER_ATTRIBUTE);
+        if(user==null || (user!=null && !user.getUserRole().equals(UserRole.ADMINISTRATOR.toString()))) {
+            try {
+                logger.info("AdminFilter ::  user has not permission to see /admdashboard.jsp ");
+                response.sendRedirect(CauliflowerInfo.HOME_LINK);
+            } catch (IOException e) {
+                logger.error(e);
+            }
+        }else{
+            try {
+                chain.doFilter(request,response);
+                logger.info("AdminFilter ::  user has permission to /admdashboard.jsp ");
+            } catch (IOException e) {
+                logger.error(e);
+            } catch (ServletException e) {
+                logger.error(e);
+            }
+        }
+    }
+}
