8e8181a33d0c41cfb255a6eb81591cce723005b0
dvorig3 <idvorskij@mail.ru>
2014-12-15 23:52:29 +0200

    avoid magic numbers fix

--- src/main/java/com/naukma/cauliflower/dao/DAO.java
+++ src/main/java/com/naukma/cauliflower/dao/DAO.java
@@ -2669,11 +2669,10 @@ public class DAO {
     /**
      * ---------------------------------------------------------------------IGOR---------------------------------------------------------------------*
      */
+	 
 	/**
 	 * Create ReportGenerator object to generate report for CIA Reports
-	 * 
-	 * @param EXT
-	 *            extension of file to generate report
+	 * @param EXT extension of file to generate report
 	 * @return ReportGenerator for
 	 * @throws java.sql.SQLException
 	 */
@@ -2682,17 +2681,18 @@ public class DAO {
 		PreparedStatement preparedStatement = null;
 		ResultSet resultSet = null;
 		ReportGenerator reportGenerator = null;
+		final String xlsExt = "xls";
+		final String selectQuery = " SELECT P.ID_ROUTER R_ID, P.ID P_ID, SI.ID SI_ID, U.ID_USER U_ID FROM  "
+				+ " ((( SERVICEINSTANCE SI INNER JOIN USERS U ON SI.ID_USER = U.ID_USER )  "
+				+ " INNER JOIN CABLE C ON C.ID = SI.ID_CABLE )  "
+				+ " INNER JOIN PORT P ON P.ID = C.ID_PORT ) "
+				+ " INNER JOIN SERVICEINSTANCESTATUS SIST ON SIST.ID =  SI.SERVICE_INSTANCE_STATUS "
+				+ " WHERE SIST.NAME = 'ACTIVE' ";
 		try {
 			preparedStatement = connection
-					.prepareStatement(""
-							+ " SELECT P.ID_ROUTER R_ID, P.ID P_ID, SI.ID SI_ID, U.ID_USER U_ID FROM  "
-							+ " ((( SERVICEINSTANCE SI INNER JOIN USERS U ON SI.ID_USER = U.ID_USER )  "
-							+ " INNER JOIN CABLE C ON C.ID = SI.ID_CABLE )  "
-							+ " INNER JOIN PORT P ON P.ID = C.ID_PORT ) "
-							+ " INNER JOIN SERVICEINSTANCESTATUS SIST ON SIST.ID =  SI.SERVICE_INSTANCE_STATUS "
-							+ " WHERE SIST.NAME = 'ACTIVE' ");
+					.prepareStatement(selectQuery);
 			resultSet = preparedStatement.executeQuery();
-			if (EXT.equals("xls")) {
+			if (EXT.equals(xlsExt)) {
 				reportGenerator = new XLSReportGenerator(" CIA Report ",
 						resultSet);
 			} else {
