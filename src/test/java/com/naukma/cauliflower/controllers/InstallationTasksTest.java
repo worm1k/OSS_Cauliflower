6e84877aa24025d4fa9ea6e30ce3fc59bc1b59c6
nikitinalexx <nirvana.22@mail.ru>
2014-12-15 19:33:00 +0200

    Completed installation controller test

--- src/test/java/com/naukma/cauliflower/controllers/InstallationTasksTest.java
+++ src/test/java/com/naukma/cauliflower/controllers/InstallationTasksTest.java
@@ -7,39 +7,37 @@ import com.naukma.cauliflower.entities.Task;
 import com.naukma.cauliflower.entities.User;
 import com.naukma.cauliflower.info.CauliflowerInfo;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import static org.easymock.EasyMock.*;
+import static org.powermock.api.support.membermodification.MemberMatcher.constructor;
+import static org.powermock.api.support.membermodification.MemberModifier.suppress;
 
 
 /**
  * Created by Алексей on 11.12.2014.
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest( DAO.class )
 public class InstallationTasksTest {
     private HttpServletRequest request = createNiceMock(HttpServletRequest.class);
     private HttpServletResponse response = createStrictMock(HttpServletResponse.class);
     private HttpSession session = createNiceMock(HttpSession.class);
-
-    @Test
-    public void testUserNull() throws Exception {
-
-        expect(request.getSession()).andReturn(session);
-        expect(session.getAttribute(CauliflowerInfo.USER_ATTRIBUTE)).andReturn(null);
-
-        response.sendRedirect(CauliflowerInfo.AUTH_LINK);
-        expectLastCall().times(1);
-
-        replay(request, response, session);
-
-        new InstallationTasksController().doPost(request, response);
-
-        verify(request, response, session);
-        reset(request, response, session);
+    private InstallationTasksController controller = null;
+    DAO dao = null;
+    {
+        suppress(constructor(DAO.class));
+        dao = PowerMock.createNiceMock(DAO.class);
+        controller = new InstallationTasksController();
+        controller.dao = dao;
     }
-
 
     @Test
     public void testTaskAndServiceNull() throws Exception {
@@ -60,11 +58,7 @@ public class InstallationTasksTest {
 
     @Test
     public void badTaskTest() throws Exception {
-        InstallationTasksController inst = new InstallationTasksController();
-
-        inst.dao = createNiceMock(DAO.class);
-        DAO dao = inst.dao;
-        expect(request.getSession()).andReturn(session).times(1);
+        expect(request.getSession()).andReturn(session);
         expect(session.getAttribute(CauliflowerInfo.USER_ATTRIBUTE)).andReturn(createMock(User.class));
         expect(request.getParameter(CauliflowerInfo.TASK_ID_PARAM)).andReturn("0");
         expect(request.getParameter(CauliflowerInfo.SERVICE_ORDER_ID)).andReturn("0");
@@ -74,7 +68,7 @@ public class InstallationTasksTest {
 
         replay(request, response, session, dao);
 
-        inst.doPost(request, response);
+        controller.doPost(request, response);
 
         verify(request, response, session, dao);
         reset(request, response, session, dao);
@@ -84,11 +78,8 @@ public class InstallationTasksTest {
     public void badUserTest() throws Exception {
         int userRoleId = 1;
         int userRoleExpected = 0;
-        InstallationTasksController inst = new InstallationTasksController();
 
-        inst.dao = createNiceMock(DAO.class);
-        DAO dao = inst.dao;
-        expect(request.getSession()).andReturn(session).times(1);
+        expect(request.getSession()).andReturn(session);
         expect(session.getAttribute(CauliflowerInfo.USER_ATTRIBUTE)).andReturn(new User(0, userRoleId, "", "", "", "", "", false));
         expect(request.getParameter(CauliflowerInfo.TASK_ID_PARAM)).andReturn("0");
         expect(request.getParameter(CauliflowerInfo.SERVICE_ORDER_ID)).andReturn("0");
@@ -99,7 +90,32 @@ public class InstallationTasksTest {
 
         replay(request, response, session, dao);
 
-        inst.doPost(request, response);
+        controller.doPost(request, response);
+
+        verify(request, response, session, dao);
+        reset(request, response, session, dao);
+    }
+
+    @Test
+    public void noFreePortsAttributeTest() throws Exception {
+        int userRoleId = 0;
+
+        expect(request.getSession()).andReturn(session).times(2);
+        expect(session.getAttribute(CauliflowerInfo.USER_ATTRIBUTE)).andReturn(new User(0, userRoleId, "", "", "", "", "", false));
+        expect(request.getParameter(isA(String.class))).andStubReturn("0");
+        expect(dao.getTaskById(anyInt())).andReturn(new Task(0, 0, 0, 0, "PROCESSING", TaskName.CREATE_CIRCUIT));
+        expect(dao.getUserRoleIdFor(UserRole.INSTALLATION_ENG)).andReturn(userRoleId);
+        expect(dao.freePortExists()).andReturn(false);
+        session.setAttribute(CauliflowerInfo.ERROR_ATTRIBUTE, CauliflowerInfo.NO_PORTS_ERROR_MESSAGE);
+        expectLastCall();
+
+
+        response.sendRedirect(CauliflowerInfo.INSTALL_ENGINEER_DASHBOARD_LINK);
+        expectLastCall();
+
+        replay(request, response, session, dao);
+
+        controller.doPost(request, response);
 
         verify(request, response, session, dao);
         reset(request, response, session, dao);
