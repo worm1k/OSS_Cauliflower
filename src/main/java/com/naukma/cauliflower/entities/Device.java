77ae13df286e69cf10f35e6939dee422848d8e71
dvorig3 <idvorskij@mail.ru>
2014-12-17 02:21:40 +0200

    entity Device

---
+++ src/main/java/com/naukma/cauliflower/entities/Device.java
@@ -0,0 +1,42 @@
+package com.naukma.cauliflower.entities;
+
+import java.io.Serializable;
+
+public class Device implements Serializable {
+
+	private int id;
+	private int occupiedPortCap;
+	private int freePortCap;
+
+	public Device(int id, int occupiedPortCap, int freePortCap) {
+		super();
+		this.id = id;
+		this.occupiedPortCap = occupiedPortCap;
+		this.freePortCap = freePortCap;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getOccupiedPortCap() {
+		return occupiedPortCap;
+	}
+
+	public void setOccupiedPortCap(int occupiedPortCap) {
+		this.occupiedPortCap = occupiedPortCap;
+	}
+
+	public int getFreePortCap() {
+		return freePortCap;
+	}
+
+	public void setFreePortCap(int freePortCap) {
+		this.freePortCap = freePortCap;
+	}
+
+}
