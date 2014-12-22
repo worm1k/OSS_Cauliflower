16e860e2910392fb354e033ee4c724e37ab22759
nikitinalexx <nirvana.22@mail.ru>
2014-12-22 16:51:46 +0200

    Final version of provisioning controller

--- webapp/src/com/naukma/cauliflower/controllers/ProvisioningTaskController.java
+++ webapp/src/com/naukma/cauliflower/controllers/ProvisioningTaskController.java
@@ -5,6 +5,7 @@ import com.naukma.cauliflower.entities.ServiceOrder;
 import com.naukma.cauliflower.entities.Task;
 import com.naukma.cauliflower.entities.User;
 import com.naukma.cauliflower.info.CauliflowerInfo;
+import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,49 +15,49 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 
+/**
+ * This servlet controls tasks completion by installation engineer. First it checks whether session user attribute
+ * and request task id parameter exist. Then servlet checks if task received is with status processing and that user
+ * role is provisioning engineer. There are three tasks that may be completed by provisioning engineer. They are: connect
+ * instance(activates service instance), modify service(changes service for service instance), disconnect instance
+ * (disconnects service instance). After one of them is processed, both task and order associated with it are marked
+ * as completed and service instance is marked as not blocked. Finally servlet redirects to provisioning engineer
+ * dashboard.
+ */
 @WebServlet(name = "ProvisioningTaskController")
 public class ProvisioningTaskController extends HttpServlet {
-    DAO dao = DAO.INSTANCE;
-    /*
-    SOW.7
-    SOW.8
-    SOW.10
-    All operations(creating cable, assigning port, instance or removing these entities is done by installation engineer)
-    This one just activates/deactivates service instance
-     */
+    private static final Logger logger = Logger.getLogger(ProvisioningTaskController.class);
+    private DAO dao = DAO.INSTANCE;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //get all required attributes and parameters
         User user = (User) request.getSession().getAttribute(CauliflowerInfo.USER_ATTRIBUTE);
         String taskIdParam = request.getParameter(CauliflowerInfo.TASK_ID_PARAM);
 
+        //make sure that user is not null
         if (user == null) {
             response.sendRedirect(CauliflowerInfo.AUTH_LINK);
+            //make sure that task id is not null, otherwise set error attribute and redirect
         } else if (taskIdParam == null) {
             request.getSession().setAttribute(CauliflowerInfo.ERROR_ATTRIBUTE, CauliflowerInfo.SYSTEM_ERROR_MESSAGE);
             response.sendRedirect(CauliflowerInfo.DASHBOARD_LINK);
         } else {
             try {
-                System.out.println("before");
                 Integer taskId = Integer.parseInt(taskIdParam);
                 Task task = dao.getTaskById(taskId);
-                {//help
-                    System.out.println(taskId);
-                    System.out.println(dao.getTaskStatus(taskId));
-                    System.out.println(user.getUserRoleId());
-                    System.out.println(dao.getUserRoleIdFor(UserRole.PROVISIONING_ENG));
-                }
 
+                //this servlet should work only if task is processing and userRole is provisioning engineer
                 if (task.getTaskStatus().equals(TaskStatus.PROCESSING.toString())  &&
-                        user.getUserRoleId() == dao.getUserRoleIdFor(UserRole.PROVISIONING_ENG)) {
+                    user.getUserRoleId() == dao.getUserRoleIdFor(UserRole.PROVISIONING_ENG)) {
 
                     ServiceOrder serviceOrder = dao.getServiceOrder(taskId);
-                    if (task.getTaskName().equals(TaskName.CONNECT_INSTANCE)) {
+                    if (task.getTaskName().equals(TaskName.CONNECT_INSTANCE)) {//connect scenario
                         dao.changeInstanceStatus(serviceOrder.getServiceInstanceId(), InstanceStatus.ACTIVE);
                     }
-                    else if (task.getTaskName().equals(TaskName.MODIFY_SERVICE)) {
+                    else if (task.getTaskName().equals(TaskName.MODIFY_SERVICE)) {//modify scenario
                         dao.changeServiceForServiceInstance(taskId, serviceOrder.getServiceInstanceId());
                     }
-                    else if (task.getTaskName().equals(TaskName.DISCONNECT_INSTANCE)) {
+                    else if (task.getTaskName().equals(TaskName.DISCONNECT_INSTANCE)) {//disconnect scenario
                         dao.changeInstanceStatus(serviceOrder.getServiceInstanceId(), InstanceStatus.DISCONNECTED);
                     }
                     dao.changeTaskStatus(taskId, TaskStatus.COMPLETED);
@@ -68,16 +69,9 @@ public class ProvisioningTaskController extends HttpServlet {
                     response.sendRedirect(CauliflowerInfo.HOME_LINK);
 
             } catch (SQLException e) {
-                e.printStackTrace();
-                response.sendRedirect(CauliflowerInfo.PROVIS_ENGINEER_DASHBOARD_LINK);
+                logger.error(e);
+                request.getSession().setAttribute(CauliflowerInfo.ERROR_ATTRIBUTE, CauliflowerInfo.SYSTEM_ERROR_MESSAGE);
             }
         }
-
-
-
-
-
     }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { }
 }
